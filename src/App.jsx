0232d77e5d08b0a35bd9692596d8e14d6e07ec73
Justin O. Odhiambo <j.odhiambo.o@edulink.local>
2026-04-28 13:24:06 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+//Import the route modules
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { useSate, useEffect } from 'react'; 
 
-function App() {
-  const [count, setCount] = useState(0)
+// TODO Import the various components
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Contacts from './components/Contacts';
+import NotFound from './components/NotFound';
+import Services from './components/Services';
+import Gallery from './components/Gallery';
+import Blog from './components/Blog';
+import Testimonials from './components/Testimonials';
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
 
-      <div className="ticks"></div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+<Router>
+<div className='container-fluid'>
+  {/* Navigation links */}
+  <nav id='main-nav'>
+    <button id="menu-toggle" aria-label="Toggle Menu"
+      onClick={toggleMenu} className={`menu-toggle ${isMenuOpen ? 'active' : ''}`}>
+      <span className='hamburger-line'></span>
+      <span className='hamburger-line'></span>
+      <span className='hamburger-line'></span>
+    </button>
+    <ul id='nav-menu' className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
+      <li><Link to="/" onClick={() => setIsMenuOpen(false)}>Home</Link></li>
+      <li><Link to="/about" onClick={() => setIsMenuOpen(false)}>About</Link></li>
+      <li><a href="#">Menu</a>
+        <ul>
+          <li><Link to="/menu/pizzas" onClick={() => setIsMenuOpen(false)}>  Pizzas</Link></li>
+          <li><Link to="/menu/pasta" onClick={() => setIsMenuOpen(false)}>Pasta</Link></li>
+          <li><Link to="/menu/salads" onClick={() => setIsMenuOpen(false)}>Salads</Link></li>
+          <li><Link to="/menu/drinks" onClick={() => setIsMenuOpen(false)}>Drinks</Link></li>
+        </ul>
+      </li>
+      <li><Link to="/contacts" onClick={() => setIsMenuOpen(false)}>Contacts</Link></li>
+      <li><Link to="/quickbites" onClick={() => setIsMenuOpen(false)}>Quick Bites</Link></li>
+      <li><Link to="/menu" onClick={() => setIsMenuOpen(false)}>Menu</Link></li>
+    </ul>
+  </nav>
+  {/* Routes for the individual pages/components */}
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/footer" element={<Footer />} />
+    <Route path="/Contacts" element={<Contacts />} />
+    <Route path="/Notfound" element={<Notfound/>} />
+    <Route path="/Services" element={<Services />} />
+    <Route path="/Gallery/*" element={<Gallery />} />
+    <Route path="/Blog/" element={< Blog />} />
+    <Route path="Testimonials" element={<Testimonials />} />
+    
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+  <Footer />
+</div>
+</Router>
+</>
+)
 }
 
 export default App
